7a6634a83d80918097a515b393ecd51a4d36cf33
Luca Virgilio <luca.virgilio95@gmail.com>
2023-06-25 23:02:08 +0200

    Create employee client

---
+++ department-service/src/main/java/lvirgilio/mservice/departmentservice/client/EmployeeClient.java
@@ -0,0 +1,15 @@
+package lvirgilio.mservice.departmentservice.client;
+
+import lvirgilio.mservice.departmentservice.entity.Employee;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
+
+import java.util.List;
+
+@HttpExchange
+public interface EmployeeClient {
+    @GetExchange("employee/department/{departmentId}")
+    public List<Employee> getEmployeeByDepartmentId(@PathVariable("departmentId") Long departmentId);
+
+}
--- department-service/src/main/java/lvirgilio/mservice/departmentservice/service/DepartmentService.java
+++ department-service/src/main/java/lvirgilio/mservice/departmentservice/service/DepartmentService.java
@@ -1,6 +1,6 @@
 package lvirgilio.mservice.departmentservice.service;
 
-import lvirgilio.mservice.departmentservice.controller.DepartmentController;
+import lvirgilio.mservice.departmentservice.client.EmployeeClient;
 import lvirgilio.mservice.departmentservice.entity.Department;
 import lvirgilio.mservice.departmentservice.repository.DepartmentRepository;
 import org.slf4j.Logger;
@@ -17,20 +17,31 @@ public class DepartmentService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DepartmentService.class);
 
+    @Autowired
+    private EmployeeClient employeeClient;
+
 
     public Department createDepartment(Department department) {
         LOGGER.info("Department add: {}", department);
-         departmentRepository.addDepartment(department);
-            return department;
+        departmentRepository.addDepartment(department);
+        return department;
     }
 
     public List<Department> findAllDepartments() {
         LOGGER.info("Department find");
-        return departmentRepository.findAll();
+        List<Department> departments = departmentRepository.findAll();
+        departments.forEach(dep -> {
+            List employees = employeeClient.getEmployeeByDepartmentId(dep.getId());
+            dep.setEmployees(employees);
+        });
+        return departments;
     }
 
     public Department findById(Long id) {
         LOGGER.info("Department find: id={}", id);
-        return departmentRepository.findById(id);
+        Department dep = departmentRepository.findById(id);
+        List employees = employeeClient.getEmployeeByDepartmentId(id);
+        dep.setEmployees(employees);
+        return dep;
     }
 }
